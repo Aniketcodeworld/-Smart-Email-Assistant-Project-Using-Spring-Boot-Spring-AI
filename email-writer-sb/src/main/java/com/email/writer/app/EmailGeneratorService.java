8e3188c35865760ff0a9ad000ed6b7c1f8da64b8
Aniket Mishra <aniket_mishra@srmap.edu.in>
2026-09-05 20:33:29 +0545

    Fix:added comments on the service layer logic

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
-
+// Service code for Email Generator service logic
 @Service
 public class EmailGeneratorService {
 
